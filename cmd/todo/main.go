adc4e7d5b7fba440ff1825b0dd1a4d28b8e14881
buguno <brunnodesouzabezerra@gmail.com>
2023-06-27 16:16:42 -0300

    ✨ feat: creating flag add in main file with static data

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/buguno/todo-cli/internal/todo"
+)
+
+const todosFile = ".todos.json"
 
 func main() {
-	fmt.Println("Hello, world!")
+	add := flag.Bool("add", false, "add a new todo")
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todosFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+
+		err := todos.Store(todosFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
 }
