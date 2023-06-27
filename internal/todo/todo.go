49485426975103e680ebcfb237aad9c01686511c
buguno <brunnodesouzabezerra@gmail.com>
2023-06-27 15:42:16 -0300

    ✨ feat: creating add todo function

---
+++ internal/todo/todo.go
@@ -0,0 +1,23 @@
+package todo
+
+import "time"
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type Todos []item
+
+func (t *Todos) Add(task string) {
+	todo := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*t = append(*t, todo)
+}
