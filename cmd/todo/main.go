63e6bb56d99482d023cd93f5935bb0a5a265fece
buguno <brunnodesouzabezerra@gmail.com>
2023-06-27 16:29:40 -0300

    ✨ feat: creating flag complete in main file

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -12,6 +12,7 @@ const todosFile = ".todos.json"
 
 func main() {
 	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
 	flag.Parse()
 
 	todos := &todo.Todos{}
@@ -30,7 +31,18 @@ func main() {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stdout, err.Error())
+			os.Exit(1)
+		}
 
+		err = todos.Store(todosFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 	default:
 		fmt.Fprintln(os.Stdout, "invalid command")
 		os.Exit(0)
